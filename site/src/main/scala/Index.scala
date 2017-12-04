c519a90f09aa2842320106c1215562b37b73afb5
Brad Fritz <brad@fewerhassles.com>
2017-12-03 21:37:27 -0500

    add bootstrap to make it less ugly
    Installed with:
    
        VER=4.0.0-beta.2
    
        cd site/src/main/assets/bootstrap/
        curl -LO https://github.com/twbs/bootstrap/raw/v$VER/dist/css/bootstrap.min.css

--- site/src/main/scala/Index.scala
+++ site/src/main/scala/Index.scala
@@ -9,13 +9,16 @@ object Index extends Renderable {
     import scalatags.Text.all._
     import scalatags.Text.tags2.title
 
+    val titleText = "Static Site with ScalaTags"
+
     html(
       head(
-        title("Static Site with ScalaTags"),
+        title(titleText),
+        link(rel := "stylesheet", href := "assets/bootstrap/bootstrap.min.css"),
       ),
       body(
-        div(
-          h1(id := "title", "Static Site with ScalaTags"),
+        div(cls := "container",
+          h1(id := "title", titleText),
           p("Pretty lame demo so far."),
         )
       )
