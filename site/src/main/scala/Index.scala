3c3e813086c87bedb48e5c4a5fc8afe94ee55d7a
Brad Fritz <brad@fewerhassles.com>
2017-12-04 07:33:04 -0500

    finish pulling in bootstrap assets from public CDNs

--- site/src/main/scala/Index.scala
+++ site/src/main/scala/Index.scala
@@ -6,8 +6,29 @@ import ba.sake.hepek.core.Renderable
 object Index extends Renderable {
   import scalatags.Text.all._
 
-  private val crossorigin = attr("crossorigin")
+  case class CdnAsset(href: String, checksum: String)
 
+  val BootstrapVersion = "4.0.0-beta.2"
+
+  val BootstrapCss = CdnAsset(
+    s"https://maxcdn.bootstrapcdn.com/bootstrap/$BootstrapVersion/css/bootstrap.min.css",
+    "sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb")
+  val JQuery = CdnAsset(
+    "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+    "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN")
+  val Popper = CdnAsset(
+    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js",
+    "sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" )
+  val BootstrapJs = CdnAsset(
+    s"https://maxcdn.bootstrapcdn.com/bootstrap/$BootstrapVersion/js/bootstrap.min.js",
+    "sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ")
+
+  private val crossorigin = attr("crossorigin")
+  private val integrity = attr("integrity")
+
+  /**
+   * Contents of `index.html`.
+   */
   override def render: String = {
     import scalatags.Text.tags2.title
 
@@ -16,13 +37,16 @@ object Index extends Renderable {
     html(
       head(
         title(titleText),
-        link(rel := "stylesheet", href := "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", crossorigin := "anonymous"),
+        link(rel := "stylesheet", href := BootstrapCss.href, integrity := BootstrapCss.checksum, crossorigin := "anonymous"),
       ),
       body(
         div(cls := "container",
           h1(id := "title", titleText),
-          p("Pretty lame demo so far."),
-        )
+          p("Slightly less lame demo."),
+        ),
+        script(`type` := "javascript" ,src := JQuery.href),
+        script(`type` := "javascript" ,src := Popper.href),
+        script(`type` := "javascript" ,src := BootstrapJs.href),
       )
     )
   }.toString()
